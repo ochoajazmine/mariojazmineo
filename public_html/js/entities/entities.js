628304447f659c270515d739b3684d52be8f7230
jazmine ochoa <jazzy.ochoa@yahoo.com>
2014-11-12 11:23:10 -0800

    working on videos for mario

---
+++ public_html/js/entities/entities.js
@@ -0,0 +1,4 @@
+// TODO
+game.playerentity = me.entity.extend({
+    
+});
---
+++ public_html/js/resources.js
@@ -0,0 +1,30 @@
+game.resources = [
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+        {name: "background-tiles",type:"image", src:"data/img/background-tiles.png"},
+        {name: "mario", type:"image", src: "data/img/player1.png"},
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	  */
+         {name: "jazzyLevel01", type:"tmx", src:"data/map/jazzyLevel04.tmx"}
+         
+         /* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+	 */	
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+	 */
+];
