2648ac449022f5f605266747946470f6bc649cfd
jazmine ochoa <jazzy.ochoa@yahoo.com>
2014-11-18 11:27:38 -0800

    finishing lunch

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -11,7 +11,7 @@ game.PlayerEntity = me.Entity.extend({
                    return (new me.Rect(0, 0, 128, 128)).toPolygon();
                }
        }]);
-         this.body.setVelocity(5,20);
+         this.body.setVelocity(5, 20);
     },  
     
     update: function(delta){
